e5aea6266e9208117863b6cea18e79b127e3faf7
jhynasa12 <hyland.justin12@gmail.com>
2015-10-29 15:21:18 -0400

    Added new Activities and functions

--- app/src/main/java/pocketteam/pocketteam/TeamListActivity.java
+++ app/src/main/java/pocketteam/pocketteam/TeamListActivity.java
@@ -1,10 +1,8 @@
 package pocketteam.pocketteam;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class TeamListActivity extends AppCompatActivity {
@@ -16,4 +14,9 @@ public class TeamListActivity extends AppCompatActivity {
 
     }
 
+    public void AddTeamOnClickEventHandler(View view) {
+        Intent addTeamIntent = new Intent(this,AddTeamActivity.class);
+        startActivity(addTeamIntent);
+
+    }
 }
