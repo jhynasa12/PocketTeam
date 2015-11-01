4a7a94e661a81708512d6a5bec3877ca1e2f7fd4
jhynasa12 <hyland.justin12@gmail.com>
2015-11-01 17:26:41 -0500

    Added a Roster List to display players on each team.

--- app/src/main/java/pocketteam/pocketteam/AddPlayerActivity.java
+++ app/src/main/java/pocketteam/pocketteam/AddPlayerActivity.java
@@ -35,11 +35,11 @@ public class AddPlayerActivity extends AppCompatActivity {
         lastName = (EditText) findViewById(R.id.last_name);
         position = (EditText) findViewById(R.id.position);
         number = (EditText) findViewById(R.id.jersey_number);
-        team = (EditText) findViewById(R.id.team_name);
+
 
 
         //create Player
-        Player player = new Player(firstName.getText().toString(), lastName.getText().toString(), position.getText().toString(), number.getText().toString(), team.getText().toString()); //creates player
+        Player player = new Player(firstName.getText().toString(), lastName.getText().toString(), position.getText().toString(), number.getText().toString(), RosterActivity.teamName); //creates player
 
         Log.d(LOG_TAG, player.getFirstName() + " " + player.getLastName() + " " + player.getPosition() + " " + player.getPlayerNumber() + " " + player.getTeamName());
 
