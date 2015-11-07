14c72cbbf70a76271847112bc9add771d87c7d42
jhynasa12 <hyland.justin12@gmail.com>
2015-11-07 15:16:53 -0500

    Image Upload Intent

--- app/src/main/java/pocketteam/pocketteam/PlayerProfileActivity.java
+++ app/src/main/java/pocketteam/pocketteam/PlayerProfileActivity.java
@@ -1,9 +1,14 @@
 package pocketteam.pocketteam;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +17,16 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
+
+import static android.app.PendingIntent.getActivity;
 
 public class PlayerProfileActivity extends AppCompatActivity {
 
+    private static final int SELECT_IMAGE = 12;
     private static int IMAGE_GALLERY = 10;
     public static String stats;
     public static String playerName;
@@ -28,12 +38,13 @@ public class PlayerProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_profile);
 
+
         playerName = getIntent().getStringExtra(RosterActivity.PLAYER_NAME);
         TextView rosterText = (TextView) findViewById(R.id.player_profile_name);
         rosterText.setText(playerName);
 
 
- //       List<Stat> stat = StatDataProvider.getData();
+        //       List<Stat> stat = StatDataProvider.getData();
 
         //Player currentPlayer = TeamList.getInstance().getTeam()
 
@@ -44,29 +55,50 @@ public class PlayerProfileActivity extends AppCompatActivity {
     }
 
 
-    public void ProfilePictureOnClickEventHandler(View view) {
-        //we want to pick an image from a gallery, specify action pic
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
-        //
-        //give me the file path to the directory
-      String path =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
-
-        //convert to URI
-        Uri picturesDirectory = Uri.parse(path);
-
-        //set data and look for files
-        galleryIntent.setDataAndType(picturesDirectory, "image/*");
-        //
-
-        //startActivity
-        startActivityForResult(galleryIntent, IMAGE_GALLERY );
-
-    }
-
-
     public void SprayChartOnClickEventHandler(View view) {
         Intent sprayChartIntent = new Intent(this, SprayChartActivity.class);
         startActivity(sprayChartIntent);
 
     }
-}
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 1234:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+            /* Now you have choosen image in Bitmap format in object "yourSelectedImage". You can use it in way you want! */
+                }
+        }
+
+    }
+
+    ;
+
+    public void btnChoosePhotoPressed(View view) {
+
+        View.OnClickListener btnChoosePhotoPressedHelper = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                final int ACTIVITY_SELECT_IMAGE = 1234;
+                startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+            }
+        };
+    }
+
+}// end Activity
