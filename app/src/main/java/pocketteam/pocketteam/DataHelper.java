b696c43643836a47d2798c6f0ff98758192372d9
MAXPAUDELO <maxpaudelo@gmail.com>
2015-11-08 15:21:15 -0500

    Adding player info into the database.

--- app/src/main/java/pocketteam/pocketteam/DataHelper.java
+++ app/src/main/java/pocketteam/pocketteam/DataHelper.java
@@ -1,5 +1,6 @@
 package pocketteam.pocketteam;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,7 +19,7 @@ public class DataHelper extends SQLiteOpenHelper {
 
     public DataHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
@@ -36,5 +37,18 @@ public class DataHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String firstName, String lastName, String position) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,firstName);
+        contentValues.put(COL_3,lastName);
+        contentValues.put(COL_4,position);
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return  true;
     }
 }
