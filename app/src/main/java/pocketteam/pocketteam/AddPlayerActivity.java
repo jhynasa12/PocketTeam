557d9d5f2a7e5c8d2bbbea1024676217f3b6abc4
jhynasa12 <hyland.justin12@gmail.com>
2015-11-09 08:57:28 -0500

    Checked if texts fields were empty so the Activities do not crash when there is no input.

--- app/src/main/java/pocketteam/pocketteam/AddPlayerActivity.java
+++ app/src/main/java/pocketteam/pocketteam/AddPlayerActivity.java
@@ -1,5 +1,6 @@
 package pocketteam.pocketteam;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPlayerActivity extends AppCompatActivity {
 
@@ -37,39 +39,43 @@ public class AddPlayerActivity extends AppCompatActivity {
         position = (EditText) findViewById(R.id.position);
         number = (EditText) findViewById(R.id.jersey_number);
 
+        if (isEmpty(firstName) || isEmpty(lastName) || isEmpty(position) || isEmpty(number) == true) {
 
+            Context context = getApplicationContext();
+            CharSequence text = "You are missing a field...";
+            int duration = Toast.LENGTH_SHORT;
 
-        //create Player
-        Player player = new Player(firstName.getText().toString(), lastName.getText().toString(), position.getText().toString(), number.getText().toString(), RosterActivity.teamName); //creates player
+            final Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
 
-        Log.d(LOG_TAG, player.getFirstName() + " " + player.getLastName() + " " + player.getPosition() + " " + player.getPlayerNumber() + " " + player.getTeamName());
+        } else {
 
-        Log.d(LOG_TAG, player.getTeamName() + ": " + TeamList.getInstance().getSize() + " " + TeamList.getInstance().getTeam(player.getTeamName()).getTeamName());
+            //create Player
+            Player player = new Player(firstName.getText().toString(), lastName.getText().toString(), position.getText().toString(), number.getText().toString(), RosterActivity.teamName); //creates player
 
-        Team currentTeam = TeamList.getInstance().getTeam(player.getTeamName());
+            Log.d(LOG_TAG, player.getFirstName() + " " + player.getLastName() + " " + player.getPosition() + " " + player.getPlayerNumber() + " " + player.getTeamName());
 
-        //add player to the Team
-        currentTeam.addPlayer(player);
+            Log.d(LOG_TAG, player.getTeamName() + ": " + TeamList.getInstance().getSize() + " " + TeamList.getInstance().getTeam(player.getTeamName()).getTeamName());
 
-        Log.d(LOG_TAG, player.getFirstName() + "'s team is " + currentTeam.getTeamName());
+            Team currentTeam = TeamList.getInstance().getTeam(player.getTeamName());
 
-        RosterActivity.playerArrayAdapter.notifyDataSetChanged();
+            //add player to the Team
+            currentTeam.addPlayer(player);
 
-        //Go to the Roster Screen
-        finish();
+            Log.d(LOG_TAG, player.getFirstName() + "'s team is " + currentTeam.getTeamName());
 
+            RosterActivity.playerArrayAdapter.notifyDataSetChanged();
 
-
-
-
-
-
-
-
-
+            //Go to the Roster Screen
+            finish();
+        }
 
     }
 
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
 
     public void btnOnClickCancelEventHandler(View view) {
 
--- app/src/main/java/pocketteam/pocketteam/AddTeamActivity.java
+++ app/src/main/java/pocketteam/pocketteam/AddTeamActivity.java
@@ -1,5 +1,6 @@
 package pocketteam.pocketteam;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static pocketteam.pocketteam.R.id.player_name;
 import static pocketteam.pocketteam.R.id.add_player_button;
@@ -28,27 +30,38 @@ public class AddTeamActivity extends AppCompatActivity {
 
     public void AddTeamClickEventHandler(View view) {
 
-        Button mAddButton;
 
-        final EditText teamName;
-
-
-        mAddButton = (Button) findViewById(R.id.add_team_button);
-        teamName = (EditText) findViewById(R.id.team_text);
+        final EditText teamName = (EditText) findViewById(R.id.team_text);
 
 
-        newTeam = new Team(teamName.getText().toString()); //creates a new team
+        if (isEmpty(teamName) == true) {
+
+            Context context = getApplicationContext();
+            CharSequence text = "You need a team name...";
+            int duration = Toast.LENGTH_SHORT;
+
+            final Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        } else {
+
+            newTeam = new Team(teamName.getText().toString()); //creates a new team
 
 
-        TeamList.getInstance().addTeam(newTeam); //add to team list
+            TeamList.getInstance().addTeam(newTeam); //add to team list
 
 
-        Log.d(LOG_TAG, newTeam.getTeamName());
-        Log.d(LOG_TAG, TeamList.getInstance().getTeam(newTeam.getTeamName()).getTeamName());
+            Log.d(LOG_TAG, newTeam.getTeamName());
+            Log.d(LOG_TAG, TeamList.getInstance().getTeam(newTeam.getTeamName()).getTeamName());
 
-        Intent teamListIntent = new Intent(this, TeamListActivity.class);
-        startActivity(teamListIntent);
-
+            Intent teamListIntent = new Intent(this, TeamListActivity.class);
+            startActivity(teamListIntent);
+        }
 
     }// end AddTeamClickEventHandler
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
 }
+
