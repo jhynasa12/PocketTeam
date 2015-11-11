2fbd85c2084a57127a611295acf4edb0bffa1edb
jhynasa12 <hyland.justin12@gmail.com>
2015-11-11 13:39:34 -0500

    Missing fields in add player

--- app/src/main/java/pocketteam/pocketteam/AddPlayerActivity.java
+++ app/src/main/java/pocketteam/pocketteam/AddPlayerActivity.java
@@ -1,5 +1,6 @@
 package pocketteam.pocketteam;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -46,31 +47,45 @@ public class AddPlayerActivity extends AppCompatActivity {
         final EditText team;
 
 
+        if (isEmpty(editFirstName) && isEmpty(editLastName) && isEmpty(editPosition) && isEmpty(editNumber) == true ) {
+
+            Context context = getApplicationContext();
+            CharSequence text = "You're missing the fields...";
+            int duration = Toast.LENGTH_SHORT;
+
+            final Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        } else {
 
 
-        //create Player
+            //create Player
 
-        Player player = new Player(editNumber.getText().toString(), editFirstName.getText().toString(), editLastName.getText().toString(), editPosition.getText().toString(), RosterActivity.teamName); //creates player
-        myDb.addPlayer(player);
-        Log.d(LOG_TAG, player.getFirstName() + " " + player.getLastName() + " " + player.getPosition() + " " + player.getPlayerNumber() + " " + player.getTeamName());
+            Player player = new Player(editFirstName.getText().toString(), editLastName.getText().toString(), editPosition.getText().toString(), editNumber.getText().toString(), RosterActivity.teamName); //creates player
+            //      myDb.addPlayer(player);
+            Log.d(LOG_TAG, player.getFirstName() + " " + player.getLastName() + " " + player.getPosition() + " " + player.getPlayerNumber() + " " + player.getTeamName());
 
-        Log.d(LOG_TAG, player.getTeamName() + ": " + TeamList.getInstance().getSize() + " " + TeamList.getInstance().getTeam(player.getTeamName()).getTeamName());
+            Log.d(LOG_TAG, player.getTeamName() + ": " + TeamList.getInstance().getSize() + " " + TeamList.getInstance().getTeam(player.getTeamName()).getTeamName());
 
-        Team currentTeam = TeamList.getInstance().getTeam(player.getTeamName());
+            Team currentTeam = TeamList.getInstance().getTeam(player.getTeamName());
 
-        //add player to the Team
-        currentTeam.addPlayer(player);
+            //add player to the Team
+            currentTeam.addPlayer(player);
 
-        Log.d(LOG_TAG, player.getFirstName() + "'s team is " + currentTeam.getTeamName());
+            Log.d(LOG_TAG, player.getFirstName() + "'s team is " + currentTeam.getTeamName());
 
-        RosterActivity.playerArrayAdapter.notifyDataSetChanged();
+            RosterActivity.playerArrayAdapter.notifyDataSetChanged();
 
-        //Go to the Roster Screen
-        finish();
+            //Go to the Roster Screen
+            finish();
+
+        }
     }
 
 
-
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
 
 
     public void btnOnClickCancelEventHandler(View view) {
